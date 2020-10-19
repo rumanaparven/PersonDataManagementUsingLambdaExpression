dd0eafaff8b47f05dd5803496f4fb29650a4620f
Rumana <rumanaparven16@gmail.com>
2020-10-19 09:10:22 +0530

    first commit

---
+++ PersonDataManagementUsingLambdaExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonDataManagementUsingLambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management Problem using Lambda Expression");
+        }
+    }
+}
