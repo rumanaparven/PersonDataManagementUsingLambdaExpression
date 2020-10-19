fd3eb3f6c96ca57b0b011427c8eef6b7aaf8f164
Rumana <rumanaparven16@gmail.com>
2020-10-19 09:53:23 +0530

    [Rumana] Add. Retrieve person details for age between 13 to 18.

--- PersonDataManagementUsingLambdaExpression/Program.cs
+++ PersonDataManagementUsingLambdaExpression/Program.cs
@@ -12,21 +12,33 @@ namespace PersonDataManagementUsingLambdaExpression
             List<Person> list = new List<Person>();
             AddDetailsToList(list);
             RetrieveTopTwoRecordsForAgeLessThanSixty(list);
+            RetrieveRecordsForAgeBetn13To18(list);
 
         }
         public static void AddDetailsToList(List<Person> list)
         {
             list.Add(new Person("123456789", "Michael", "abcdef", 35));
-            list.Add(new Person("987654321", "Johny", "fcabdgr", 70));
-            list.Add(new Person("987654321", "Tom", "fcabdgr", 59));
+            list.Add(new Person("987654321", "Johny", "fcabdgr", 15));
+            list.Add(new Person("987654321", "Tom", "fcabdgr", 17));
             list.Add(new Person("987654321", "Samuel", "fcabdgr", 45));
         }
         public static void RetrieveTopTwoRecordsForAgeLessThanSixty(List<Person> list)
         {
-            foreach(Person p in list.FindAll(e => (e.age) < 60).Take(2).ToList())
+            Console.WriteLine("Top two persons in the list with age less than 60 : ");
+            foreach (Person p in list.FindAll(e => (e.age) < 60).Take(2).ToList())
+            {
+                Console.WriteLine("Namme : " + p.name + " Age : " + p.age);
+            }
+            Console.WriteLine(" \n");
+        }
+        public static void RetrieveRecordsForAgeBetn13To18(List<Person> list)
+        {
+            Console.WriteLine("Person with age between 13 and 18 : ");
+            foreach (Person p in list.FindAll(e => (e.age) >=13 && (e.age)<=18).ToList())
             {
                 Console.WriteLine("Namme : " + p.name + " Age : " + p.age);
             }
         }
     }
+
 }
