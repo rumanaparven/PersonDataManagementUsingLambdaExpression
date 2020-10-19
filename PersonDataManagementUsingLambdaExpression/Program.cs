9c7d871445de60e14c06afc4323045f15e9c95d2
Rumana <rumanaparven16@gmail.com>
2020-10-19 09:45:51 +0530

    [Rumana] Add. Retrieve top 2 details with age less than 60.

--- PersonDataManagementUsingLambdaExpression/Program.cs
+++ PersonDataManagementUsingLambdaExpression/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonDataManagementUsingLambdaExpression
 {
@@ -10,13 +11,22 @@ namespace PersonDataManagementUsingLambdaExpression
             Console.WriteLine("Welcome to Person Data Management Problem using Lambda Expression");
             List<Person> list = new List<Person>();
             AddDetailsToList(list);
+            RetrieveTopTwoRecordsForAgeLessThanSixty(list);
 
         }
         public static void AddDetailsToList(List<Person> list)
         {
             list.Add(new Person("123456789", "Michael", "abcdef", 35));
             list.Add(new Person("987654321", "Johny", "fcabdgr", 70));
-            Console.ReadLine();
+            list.Add(new Person("987654321", "Tom", "fcabdgr", 59));
+            list.Add(new Person("987654321", "Samuel", "fcabdgr", 45));
+        }
+        public static void RetrieveTopTwoRecordsForAgeLessThanSixty(List<Person> list)
+        {
+            foreach(Person p in list.FindAll(e => (e.age) < 60).Take(2).ToList())
+            {
+                Console.WriteLine("Namme : " + p.name + " Age : " + p.age);
+            }
         }
     }
 }
