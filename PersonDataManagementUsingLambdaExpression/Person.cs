ce98380549c060162d018ab2bc8643495a019837
Rumana <rumanaparven16@gmail.com>
2020-10-19 09:21:10 +0530

    [Rumana] Add. Details added to list.

---
+++ PersonDataManagementUsingLambdaExpression/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagementUsingLambdaExpression
+{
+    class Person
+    {
+        public string SSN;
+        public string name;
+        public string address;
+        public int age;
+        public Person(string SSN, string name, string address, int age)
+        {
+            this.SSN = SSN;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+    }
+}
--- PersonDataManagementUsingLambdaExpression/Program.cs
+++ PersonDataManagementUsingLambdaExpression/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PersonDataManagementUsingLambdaExpression
 {
@@ -7,6 +8,15 @@ namespace PersonDataManagementUsingLambdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Person Data Management Problem using Lambda Expression");
+            List<Person> list = new List<Person>();
+            AddDetailsToList(list);
+
+        }
+        public static void AddDetailsToList(List<Person> list)
+        {
+            list.Add(new Person("123456789", "Michael", "abcdef", 35));
+            list.Add(new Person("987654321", "Johny", "fcabdgr", 70));
+            Console.ReadLine();
         }
     }
 }
